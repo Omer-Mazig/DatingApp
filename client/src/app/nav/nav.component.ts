9c8ef24f7cabdac448d2679521c9dd03c2b4f7d0
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-08-13 09:33:10 +0300

    added login and logout http to the client

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn = false;
   model: any = {};
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: (error) => console.log(error),
+    });
+  }
+  logout() {
+    this.loggedIn = false;
   }
 }
